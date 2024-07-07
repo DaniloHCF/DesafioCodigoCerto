486360e43bba9f04769d83dd488a023f4a7422f1
danilohcf@hotmail.com <danilohcf@hotmail.com>
2024-07-07 01:32:49 -0300

    Iniciando o projeto após criação no Spring initializr

---
+++ src/test/java/com/certo/codigo/desafioBackEnd/DesafioBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certo.codigo.desafioBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
